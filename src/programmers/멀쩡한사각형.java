861f56a2bff99e4ccf9d8ee35fd50144bd41b873
nelljun <nelljun.kim@gmail.com>
2022-03-07 09:01:41 +0900

    initial commit

---
+++ src/programmers/멀쩡한사각형.java
@@ -0,0 +1,27 @@
+package src.programmers;
+
+public class 멀쩡한사각형 {
+
+    public static void main(String[] args) {
+        solution(12, 8);
+    }//main() end
+
+    public static long solution(int w, int h) {
+        //가로로 한칸씩 움직이면서 대각선이 지나는 위치(y좌표)를 보고
+        //지나가는 사각형의 개수를 계속 더해준다.
+
+        //각 구간의 처음 지나가는 사각형의 위치
+        //현재 대각선의 y좌표가 2.5라면 2 이후의 사각형부터 지나간다.
+        int prevFlr = 0;
+        int sumOfUnUsedSqr = 0;
+
+        for(int i=1; i<=w; i++) {
+            //한 칸 이동한 후 대각선의 y좌표를 포함한 사각형서부터
+            //시작 위치 사각형까지 빼주면 한 칸 이동할 때 지나간 사각형의 개수다.
+            sumOfUnUsedSqr += Math.ceil((long)h*i/(double)w)-prevFlr;
+            prevFlr = (int)Math.floor((long)h*i/(double)w);
+        }//for end
+
+        return w*(long)h - sumOfUnUsedSqr;
+    }//solution() end
+}
