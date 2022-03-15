9019118808d23f5e7575680c7f08a82101a37c31
nelljun <nelljun.kim@gmail.com>
2022-03-15 13:05:30 +0900

    initial commit

--- src/programmers/짝지어제거하기.java
+++ src/programmers/짝지어제거하기.java
@@ -1,10 +1,27 @@
-package src.programmers;public class 짝지어제거하기 {
+package src.programmers;
+
+import java.util.Stack;
+
+public class 짝지어제거하기 {
 
     public static void main(String[] args) {
-    
+
     }//main() end
-    
-    public static void solution() {
-    
+
+    public static int solution(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        int length = s.length();
+
+        for(int i=0; i<length; i++) {
+            char strChar = s.charAt(i);
+            if(!stk.isEmpty() && stk.peek()==strChar) {
+                stk.pop();
+            } else {
+                stk.push(strChar);
+            }//if~else end
+        }//for end
+
+        return stk.isEmpty()? 1 : 0;
     }//solution() end
 }
