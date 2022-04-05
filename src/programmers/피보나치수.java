9c5f94335026dd5834d35946c17b062ba7d23370
nelljun <nelljun.kim@gmail.com>
2022-04-05 10:08:39 +0900

    update code

--- src/programmers/피보나치수.java
+++ src/programmers/피보나치수.java
@@ -6,7 +6,6 @@ public class 피보나치수 {
     }
 
     public static int solution(int n) {
-
         int temp1 = 0, temp2 = 1, temp3 = 0;
 
         for (int i = 2; i <= n; i++) {
@@ -17,12 +16,4 @@ public class 피보나치수 {
 
         return temp3;
     }//solution() end
-
-    public static int fibonachi(int n) {
-        if (n==0) return 0;
-        if (n==1) return 1;
-
-        return (fibonachi(n-2) + fibonachi(n-1)) % 1234567;
-    }
-
 }
