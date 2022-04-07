07dad2e63272f54178251d2135d8e4940d8dbf3a
nelljun <nelljun.kim@gmail.com>
2022-04-07 14:18:17 +0900

    initial commit

--- src/programmers/위장.java
+++ src/programmers/위장.java
@@ -1,33 +1,37 @@
 package src.programmers;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 public class 위장 {
 
-	public int solution(String[][] clothes) {
-        int answer = 1;
-        
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        
-        for(int i=0; i<clothes.length; i++) {
-        	hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0)+1);
-        }//의상의 종류를 value, 해당 종류의 갯수를 key로 저장
-        
-//        Set<String> keySet = hashMap.keySet();
-//        Iterator<String> keyIterator = keySet.iterator();
-        //value iterator
-        Iterator<Integer> iterator = hashMap.values().iterator();
-        
-        //intValue() vs parseInt()
-        while(iterator.hasNext()) {
-        	answer *=  iterator.next().intValue()+1;
-        }//while() end
-        
-        //모든 옷의 종류에 대해서 입지 않는 경우를 포함하여 곱한 다음에
-        //모든 옷을 입지 않은 경우를 빼주면 전체 경우의 수가 나온다.
-        
-        return answer-1;
+    public static void main(String[] args) {
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        solution(clothes);
     }
-	
+    public static int solution(String[][] clothes) {
+        //<종류, 해당 종류 옷의 갯수> 형식의 map구조
+        HashMap<String, Integer> clothesMap = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            //key가 cloth[1]인 value값이 있으면 그 값의 + 1하고,
+            //없으면 0에 +1 한다.
+            clothesMap.put(cloth[1], clothesMap.getOrDefault(cloth[1], 0)+1);
+        }//for end
+
+        //map 탐색할 iterator
+        Iterator<String> iterator = clothesMap.keySet().iterator();
+
+        /**
+         * 어떤 종류의 옷이 i개라면,
+         * 해당 종류에서 나오는 경우의 수는 안 입는 경우 포함해서 (i+1)개.
+         * 각 종류에 대해 (개수+1)개 값 곱한 후에, 모든 종류를 안 입는 경우 1개를 빼주면 답
+         */
+        int answer = 1;
+        while (iterator.hasNext()) {
+            answer *= clothesMap.get(iterator.next())+1;
+        }//while end
+
+        return answer-1;
+
+    }//solution() end
 }
