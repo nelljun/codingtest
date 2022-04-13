ad1dc9629c8a44cbe91999a56b7e44d8778818a8
nelljun <nelljun.kim@gmail.com>
2022-04-13 21:37:50 +0900

    add dfs solution

--- src/programmers/등굣길.java
+++ src/programmers/등굣길.java
@@ -4,7 +4,7 @@ public class 등굣길 {
     public static void main(String[] args) {
 
     }
-
+    //dp
     public static int solution(int m, int n, int[][] puddles) {
         int[][] map = new int[n][m];
 
@@ -35,4 +35,39 @@ public class 등굣길 {
 
         return map[n-1][m-1];
     }//solution() end
+
+    static int[][] board;
+    static int answer = 0;
+    static final int[] DIRECTIONS_X = {1, 0};
+    static final int[] DIRECTION_Y = {0, 1};
+
+    //dfs(정확성 ok, 효율성 x)
+    public static int solution2(int m, int n, int[][] puddles) {
+        board = new int[n][m];
+
+        //웅덩이 -1로 표시
+        for (int[] puddle : puddles) {
+            board[puddle[1]-1][puddle[0]-1] = -1;
+        }//for end
+
+        dfs(0, 0, m, n);
+
+        return answer;
+    }//solution() end
+
+    public static void dfs(int x, int y, int m, int n) {
+        if (x==m-1 && y==n-1) {
+            answer = (answer+1)%1000000007;
+            return;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            int newX = x + DIRECTIONS_X[i];
+            int newY = y + DIRECTION_Y[i];
+
+            if (newX<m && newY<n && board[newY][newX]!=-1) {
+                dfs(newX, newY, m, n);
+            }
+        }//for end
+    }//dfs() end
 }
