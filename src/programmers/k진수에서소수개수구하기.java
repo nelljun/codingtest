aa49223ef6e1b4c4fd6b117e1a70e6c3a7d68487
nelljun <nelljun.kim@gmail.com>
2022-05-03 23:54:22 +0900

    add comments

--- src/programmers/k진수에서소수개수구하기.java
+++ src/programmers/k진수에서소수개수구하기.java
@@ -15,6 +15,7 @@ public class k진수에서소수개수구하기 {
         }//while end
 
         String result = sb.toString();
+        //1개 이상의 0("0+")을 기준으로 분할
         String[] arrWithNoZero = result.split("0+");
 
         int answer = 0;
@@ -27,11 +28,15 @@ public class k진수에서소수개수구하기 {
         return answer;
     }//solution() end
 
+    /**
+     * @param num : long형, k진법으로 나타낸 수를 그대로 10진법으로 봤을 때 Integer의 범위 넘어갈 수 있다.
+     */
+    //소수 판별하는 method
     public static boolean isPrime(long num) {
         if (num==2) return true;
         if (num==1 || num%2==0) return false;
 
-        for (int i = 3; i*(long)i < num; i+=2) {
+        for (int i = 3; i*(long)i <= num; i+=2) {
             if (num%i==0) return false;
         }//for end
 
