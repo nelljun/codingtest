fc1ed0e6b693568ea8f7fde37d677b7050bfcdf1
nelljun <nelljun.kim@gmail.com>
2022-06-17 00:17:31 +0900

    initial commit

--- src/baekjoon/Main9081.java
+++ src/baekjoon/Main9081.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Main9081 {
     public static void main(String[] args) throws IOException {
@@ -13,15 +15,27 @@ public class Main9081 {
 
         StringBuilder sb = new StringBuilder();
 
-        ArrayList<Character> charList = new ArrayList<>();
+        LinkedList<Character> charList = new LinkedList<>();
+
+        /**
+         * 1. 뒤에서부터 바로 앞 요소와 비교한다.
+         * 1-1 바로 앞 요소가 더 크거나 같으면 다음 요소로 넘어간다.
+         * 1-2 바로 앞 요소가 더 작으면 아래 과정을 진행한다.
+         *
+         * 2. 바로 앞 요소를, 여태까지 지나왔던 요소들 중 바로 앞 요소보다 큰 수 중 최솟값으로 바꾼다.
+         * 3. 그 뒤는 오름차순으로 정렬
+         */
+
         for (int i = 0; i < totalCnt; i++) {
+            //list 초기화
             charList.clear();
 
             String word = bf.readLine();
             int length = word.length();
 
+            //문자를 list에 저장
             for (int j = 0; j < length; j++) {
-                charList.add(word.charAt(i));
+                charList.add(word.charAt(j));
             }//for end
 
             for (int j = length-1; j >= 1; j--) {
@@ -41,24 +55,28 @@ public class Main9081 {
                         }//if end
                     }//for end
 
+                    //prev 위치에 위에서 찾은 prev보다 큰 최솟값 넣기
+                    charList.remove(greaterMinIdx);
                     charList.remove(j-1);
                     charList.add(j-1, greaterMin);
                     charList.add(prev);
 
+                    //j~length-1 위치에 있는 문자들을 오름차순으로 정렬
+                    Collections.sort(charList.subList(j, length));
 
                     break;
-                }
+                }//if end
             }//for end
 
-            /**
-             * 1. 뒤에서부터 바로 앞 요소와 비교한다.
-             * 1-1 바로 앞 요소가 더 크거나 같으면 다음 요소로 넘어간다.
-             * 1-2 바로 앞 요소가 더 작으면 아래 과정을 진행한다.
-             *
-             * 2. 바로 앞 요소를, 여태까지 지나왔던 요소들 중 바로 앞 요소보다 큰 수 중 최솟값으로 바꾼다.
-             * 3. 그 뒤는 최소값으로 만든다.
-             */
+            //정렬한 list 순서대로 문자 출력
+            for (Character character : charList) {
+                sb.append(character);
+            }//for end
 
+            //줄바꿈
+            sb.append("\n");
         }//for end
+
+        System.out.println(sb);
     }
 }
