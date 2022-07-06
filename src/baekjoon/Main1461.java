ed4d86ff364bc6dd4a28ed0fc1fa8b5aca9eb3db
nelljun <nelljun.kim@gmail.com>
2022-07-06 23:42:25 +0900

    intial commit

---
+++ src/baekjoon/Main1461.java
@@ -0,0 +1,17 @@
+package src.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+    }
+}
