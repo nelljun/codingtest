20e860f88e79d695d7869a0de0e23ea5b39e720d
nelljun <nelljun.kim@gmail.com>
2022-07-21 23:50:19 +0900

    initial commit

---
+++ src/baekjoon/Main2206.java
@@ -0,0 +1,14 @@
+package src.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main2206 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+    }
+}
