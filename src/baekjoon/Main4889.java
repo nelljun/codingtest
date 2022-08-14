876532fc27a4897b9b118bd3469d1ef106ed9113
nelljun <nelljun.kim@gmail.com>
2022-08-14 21:09:09 +0900

    update solution

--- src/baekjoon/Main4889.java
+++ src/baekjoon/Main4889.java
@@ -3,55 +3,27 @@ package src.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main4889 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        Stack<Character> stk = new Stack<>();
         StringBuilder sb = new StringBuilder();
 
         int index = 1;
 
         while (true) {
             String str = bf.readLine();
-            int length = str.length();
-
-            int answer = 0;
 
             if (str.contains("-")) {
                 break;
             }//if end
 
-            //stack 초기화
-            stk.clear();
-
-            for (int i = 0; i < length; i++) {
-                char now = str.charAt(i);
-
-                switch (now) {
-                    case '{' :
-                        stk.push(now);
-                        break;
-                    case '}' :
-                        if (stk.isEmpty()) {
-                            answer++;
-                            stk.push('{');
-                        } else {
-                            stk.pop();
-                        }//if~else end
-                        break;
-                }
-            }//for end
-
-            int left = stk.size();
-
-            answer += (left / 2);
+            int count = count(str);
 
             sb.append(index)
                 .append(". ")
-                .append(answer)
+                .append(count)
                 .append("\n");
 
             index++;
@@ -59,4 +31,34 @@ public class Main4889 {
 
         System.out.println(sb);
     }
+
+    private static int count(String str) {
+        int answer = 0;
+        int length = str.length();
+
+        int num = 0;
+
+        for (int i = 0; i < length; i++) {
+            char now = str.charAt(i);
+
+            switch (now) {
+                case '{' :
+                    num++;
+                    break;
+                case '}' :
+                    if (num==0) {
+                        answer++;
+                        num++;
+                    } else {
+                        num--;
+                    }//if~else end
+                    break;
+            }//switch case end
+        }//for end
+
+        answer += (num / 2);
+
+        return answer;
+    }//count() end
+
 }
