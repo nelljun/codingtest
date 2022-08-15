9b037be5aa401314e3f00f5c5340e0b08b0f3d18
nelljun <nelljun.kim@gmail.com>
2022-08-15 22:42:30 +0900

    initial commit

---
+++ src/baekjoon/Main5525.java
@@ -0,0 +1,42 @@
+package src.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main5525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+
+        String S = bf.readLine();
+
+        String target = makeTarget(N);
+
+        int answer = 0;
+
+        for (int i = 0; i < M-(2*N+1); i++) {
+            if (S.charAt(i) == 'I') {
+                if (target.equals(S.substring(i, i+(2*N+1)))) {
+                    answer++;
+                }//if end
+            }//if end
+        }//for end
+
+        System.out.println(answer);
+    }
+
+    private static String makeTarget(int N) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("I");
+
+        for (int i = 0; i < N; i++) {
+            sb.append("OI");
+        }//for end
+
+        return sb.toString();
+    }
+}
